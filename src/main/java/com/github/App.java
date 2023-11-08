682d9b56b416449e753b3bbea3e13cb283c3b10c
bhargav <bhargav.c@codetru.com>
2023-11-08 15:20:24 +0530

    Main

--- src/main/java/com/github/App.java
+++ src/main/java/com/github/App.java
@@ -8,18 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Bhargav");
-        System.out.println("Gunadeep");
-        System.out.println("Bhargav");
-        System.out.println("Bhargav");
-        System.out.println("Bhargav");
-        System.out.println("Bhargav");
-        System.out.println("Bhargav");
-System.out.println("CHNAGED");
-        System.out.println("Bhargav");
-        System.out.println("Bhargav");
-        System.out.println("CHNAGED");
-        System.out.println("CHNAGED");System.out.println("CHNAGED");
-        System.out.println("CHNAGED");
+        System.out.println("MAIN");
     }
+    
 }
